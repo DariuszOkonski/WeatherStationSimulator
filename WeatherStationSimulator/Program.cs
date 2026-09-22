2a9d4c4f7e69ece2d46a9f524e87fdec7a98f19e
Dariusz Okonski <darek200180@gmail.com>
2026-09-22 06:57:02 +0200

    initialization

---
+++ WeatherStationSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherStationSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();
+        }
+    }
+}
