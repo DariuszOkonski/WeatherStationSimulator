69e52b002ad79daa33dd2b1debd26b8c59928db9
Dariusz Okonski <darek200180@gmail.com>
2026-09-22 07:02:24 +0200

    add arrays

--- WeatherStationSimulator/Program.cs
+++ WeatherStationSimulator/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the number of days to simulate: ");
+            int days = int.Parse(Console.ReadLine());
+
+            int[] temperature = new int[days];
+            string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy" };
+            string[] weatherConditions = new string[days];
+
+            Random random = new Random();
+
+
 
             Console.ReadKey();
         }
